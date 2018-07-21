ce8dd1fc03fccb822ed52b8f7baa487cc4be7f3e
Manish Kumar <manish@trisysit.com>
2018-07-21 23:30:34 +0530

    Latest Dump

--- DBDump/21072018.sql
+++ DBDump/21072018.sql
@@ -120,7 +120,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES ('618ff242-aff9-11e7-9c54-c0d962399766','REPORT','ROLE_REPORT'),('9318c4f6-ffda-2222-8b94-062a8f2c183b','UPLOAD','ROLE_UPLOAD'),('9318c4f6-ffda-5555-8b94-062a8f2c183b','User Role','ROLE_USER_ROLE'),('9318c4f6-ffda-6666-8b94-062a8f2c183b','Users','ROLE_USERS');
+INSERT INTO `role` VALUES ('618ff242-aff9-11e7-9c54-c0d962399766','ROLE ADMIN','ROLE_ADMIN'),('9318c4f6-ffda-2222-8b94-062a8f2c183b','ROLE SERVER ADMIN','ROLE_SERVER'),('9318c4f6-ffda-5555-8b94-062a8f2c183b','User Role','ROLE_USER_ROLE'),('9318c4f6-ffda-6666-8b94-062a8f2c183b','Users','ROLE_USERS');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -199,9 +199,9 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `enabled` varchar(255) DEFAULT '1',
   `createdBy` varchar(255) NOT NULL,
-  `updatedBy` varchar(255) NOT NULL,
+  `updatedBy` varchar(255) DEFAULT NULL,
   `createdDate` varchar(255) NOT NULL,
-  `updatedDate` varchar(255) NOT NULL,
+  `updatedDate` varchar(255) DEFAULT NULL,
   `status` varchar(255) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
@@ -218,7 +218,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('1c3b551c-15e7-443d-8d1a-2cd08f3d8d90','s@trisysit.com','987778789987898','eff2b051-8f55-3fc9-9a39-f72816e02056','$2a$10$uMq00ybBdNiMaMG54GX2UuKcG0VAXmvvUjwcZz2jN3q1YXnW12uFe','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','','20-10-2017','','ACTIVE','s','s',NULL,NULL),('2da27889-b437-40f6-808b-54926b935053','d@d.com','412333111131331','6e9d5b8b-eab0-3295-844c-b95bbe973c66','$2a$10$DAK67ET9nGEYOOmsP7SW6OfNI8IC918gdkIieA7yoaFQwgD7Ln50u','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','','19-10-2017','','ACTIVE','F','G',NULL,NULL),('2da8a06e-84db-4692-bf10-ff9ee3366c84','s@s.com','87887879879898','0df5a2fe-05bc-341b-99f1-285e608d8055','$2a$10$/ZnxB8mqUhuXV9nd/oqhEuW0b9THNWb.tiVSjg6.Ku/HZf8mivcJq','0','5013f26e-3e05-4dd3-8cf7-9312a4915755','','20-10-2017','','ACTIVE','s','s',NULL,NULL),('5013f26e-3e05-4dd3-8cf7-9312a4915755','admin','','','$2a$10$cYLM.qoXpeAzcZhJ3oXRLu9Slkb61LHyWW5qJ4QKvHEMhaxZ5qCPi','1','','','2016-12-27 06:09:22','2017-04-10 16:43:56','ACTIVE','MacKellar','Admin','',NULL),('9ff0e108-11a1-48f6-a4b3-3a3ffc018a9f','r@r.com','888888888888888','e9393115-e87a-3fa9-801e-c1b8744b093a','$2a$10$1ikpUIZRyKD4iCfuuYft/uc8ov3Uck3GYFTzSFgm7znqfRXpdoLk.','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','5013f26e-3e05-4dd3-8cf7-9312a4915755','19-10-2017','19-10-2017','ACTIVE','reggie','ried',NULL,NULL),('ca55efc0-346b-3f3c-8fce-dcbcdb8e56ba','report','','','$2a$10$ZCm6gxQl9Kab0nIkCseQpe4KMfphemXTWIeH/KrlSI/.XOSJn5zaC','1','','','2016-12-27 06:09:22','2017-04-10 16:43:56','ACTIVE','Report','',NULL,NULL),('f660c970-4d61-43bc-9b3a-b98aa44aeed9','sumit@t.com','988897897989878','a1b67dd2-c036-31ff-b86e-4350ff659826','$2a$10$chemfHAa73Wmpfq54.VhZerEk4J7Qrj4XqyZfj1bt3hqMRYvXs6sK','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','','20-10-2017','','ACTIVE','sumit','dey',NULL,NULL);
+INSERT INTO `user` VALUES ('1887e5ed-ada9-4b29-86fa-f80076cf5b7c','765mann@gmail.com','9167762740','9653e067-2544-364d-8ab0-7ffe1951fb10','$2a$10$ZGmgLiXF81Kh.bZWY4dcUOTQAzdjdVQe6LYjotufYsIudVLWr0Lrq','1','5013f26e-3e05-4dd3-8cf7-9312a4915755',NULL,'21-07-2018',NULL,'ACTIVE','Manish','Kumar',NULL,NULL),('1c3b551c-15e7-443d-8d1a-2cd08f3d8d90','s@trisysit.com','987778789987898','eff2b051-8f55-3fc9-9a39-f72816e02056','$2a$10$uMq00ybBdNiMaMG54GX2UuKcG0VAXmvvUjwcZz2jN3q1YXnW12uFe','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','','20-10-2017','','ACTIVE','s','s',NULL,NULL),('2da27889-b437-40f6-808b-54926b935053','d@d.com','412333111131331','6e9d5b8b-eab0-3295-844c-b95bbe973c66','$2a$10$DAK67ET9nGEYOOmsP7SW6OfNI8IC918gdkIieA7yoaFQwgD7Ln50u','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','','19-10-2017','','ACTIVE','F','G',NULL,NULL),('2da8a06e-84db-4692-bf10-ff9ee3366c84','s@s.com','87887879879898','0df5a2fe-05bc-341b-99f1-285e608d8055','$2a$10$/ZnxB8mqUhuXV9nd/oqhEuW0b9THNWb.tiVSjg6.Ku/HZf8mivcJq','0','5013f26e-3e05-4dd3-8cf7-9312a4915755','','20-10-2017','','ACTIVE','s','s',NULL,NULL),('5013f26e-3e05-4dd3-8cf7-9312a4915755','admin','','','$2a$10$cYLM.qoXpeAzcZhJ3oXRLu9Slkb61LHyWW5qJ4QKvHEMhaxZ5qCPi','1','','','2016-12-27 06:09:22','2017-04-10 16:43:56','ACTIVE','MacKellar','Admin','',NULL),('9ff0e108-11a1-48f6-a4b3-3a3ffc018a9f','r@r.com','888888888888888','e9393115-e87a-3fa9-801e-c1b8744b093a','$2a$10$1ikpUIZRyKD4iCfuuYft/uc8ov3Uck3GYFTzSFgm7znqfRXpdoLk.','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','5013f26e-3e05-4dd3-8cf7-9312a4915755','19-10-2017','19-10-2017','ACTIVE','reggie','ried',NULL,NULL),('ca55efc0-346b-3f3c-8fce-dcbcdb8e56ba','report','','','$2a$10$ZCm6gxQl9Kab0nIkCseQpe4KMfphemXTWIeH/KrlSI/.XOSJn5zaC','1','','','2016-12-27 06:09:22','2017-04-10 16:43:56','ACTIVE','Report','',NULL,NULL),('f660c970-4d61-43bc-9b3a-b98aa44aeed9','sumit@t.com','988897897989878','a1b67dd2-c036-31ff-b86e-4350ff659826','$2a$10$chemfHAa73Wmpfq54.VhZerEk4J7Qrj4XqyZfj1bt3hqMRYvXs6sK','1','5013f26e-3e05-4dd3-8cf7-9312a4915755','','20-10-2017','','ACTIVE','sumit','dey',NULL,NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -243,7 +243,7 @@ CREATE TABLE `usermasterrole` (
 
 LOCK TABLES `usermasterrole` WRITE;
 /*!40000 ALTER TABLE `usermasterrole` DISABLE KEYS */;
-INSERT INTO `usermasterrole` VALUES ('465a67fe-b54b-11e7-8cb9-c6596e1a37c5','1c3b551c-15e7-443d-8d1a-2cd08f3d8d90','9318c4f6-ffda-11e6-8b94-062a8f2c000b'),('9a31870c-b4f8-11e7-8cb9-c6596e1a37c5','9ff0e108-11a1-48f6-a4b3-3a3ffc018a9f','9318c4f6-ffda-11e6-8b94-062a8f2c111b'),('a34fdfea-b549-11e7-8cb9-c6596e1a37c5','2da8a06e-84db-4692-bf10-ff9ee3366c84','9318c4f6-ffda-11e6-8b94-062a8f2c000b'),('e6ee7c80-b548-11e7-8cb9-c6596e1a37c5','f660c970-4d61-43bc-9b3a-b98aa44aeed9','9318c4f6-ffda-11e6-8b94-062a8f2c111b'),('f368d672-9666-4cd1-b0c6-62694bd4da31','5013f26e-3e05-4dd3-8cf7-9312a4915755','9318c4f6-ffda-11e6-8b94-062a8f2c000b'),('f408f1e6-b4fa-11e7-8cb9-c6596e1a37c5','2da27889-b437-40f6-808b-54926b935053','9318c4f6-ffda-11e6-8b94-062a8f2c111b');
+INSERT INTO `usermasterrole` VALUES ('465a67fe-b54b-11e7-8cb9-c6596e1a37c5','1c3b551c-15e7-443d-8d1a-2cd08f3d8d90','9318c4f6-ffda-11e6-8b94-062a8f2c000b'),('9a31870c-b4f8-11e7-8cb9-c6596e1a37c5','9ff0e108-11a1-48f6-a4b3-3a3ffc018a9f','9318c4f6-ffda-11e6-8b94-062a8f2c111b'),('a229dcac-8d0e-11e8-b743-4439c45b4ecf','1887e5ed-ada9-4b29-86fa-f80076cf5b7c','9318c4f6-ffda-11e6-8b94-062a8f2c111b'),('a34fdfea-b549-11e7-8cb9-c6596e1a37c5','2da8a06e-84db-4692-bf10-ff9ee3366c84','9318c4f6-ffda-11e6-8b94-062a8f2c000b'),('e6ee7c80-b548-11e7-8cb9-c6596e1a37c5','f660c970-4d61-43bc-9b3a-b98aa44aeed9','9318c4f6-ffda-11e6-8b94-062a8f2c111b'),('f368d672-9666-4cd1-b0c6-62694bd4da31','5013f26e-3e05-4dd3-8cf7-9312a4915755','9318c4f6-ffda-11e6-8b94-062a8f2c000b'),('f408f1e6-b4fa-11e7-8cb9-c6596e1a37c5','2da27889-b437-40f6-808b-54926b935053','9318c4f6-ffda-11e6-8b94-062a8f2c111b');
 /*!40000 ALTER TABLE `usermasterrole` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -284,4 +284,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-21 14:35:40
+-- Dump completed on 2018-07-21 23:29:13
